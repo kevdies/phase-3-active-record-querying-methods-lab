72f877b1361b1f6b3d580cd8533ed8a8b6cde80c
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-18 18:58:59 -0500

    slowly understanding migrations and where to type code

--- db/migrate/20221218231141_add_season_to_shows.rb
+++ db/migrate/20221218231141_add_season_to_shows.rb
@@ -1,4 +1,4 @@
-class CreateAddSeasonToShows < ActiveRecord::Migration[6.1]
+class AddSeasonToShows < ActiveRecord::Migration[6.1]
   def change
     #i need to add a column to the shows table, im getting an error that there isn't a shows table???
     add_column :shows, :season, :string
