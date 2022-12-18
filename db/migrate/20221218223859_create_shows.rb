69e1a1c60dea251f23e9295d0617b4245aa406af
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-18 18:38:58 -0500

    getting an error saying there isn't a table called shows.  schema shows the add on second migration went thru. not sure why this error is happening

--- db/migrate/20221218223859_create_shows.rb
+++ db/migrate/20221218223859_create_shows.rb
@@ -5,7 +5,6 @@ class CreateShows < ActiveRecord::Migration[6.1]
       t.string :network
       t.string :day
       t.integer :rating
-      t
     end
   end
 end
---
+++ db/migrate/20221218231141_create_add_season_to_shows.rb
@@ -0,0 +1,6 @@
+class CreateAddSeasonToShows < ActiveRecord::Migration[6.1]
+  def change
+    #i need to add a column to the shows table, im getting an error that there isn't a shows table???
+    add_column :shows, :season, :string
+  end
+end
